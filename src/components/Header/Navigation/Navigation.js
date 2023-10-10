400ada0fd5a163c32fb9461621cac8d019934adb
Shnaider Danila <shnd3r@yandex.ru>
2023-10-10 13:52:29 +0700

    feat: Add the NavLink component from the Router-dom library to ./src/components/Header/Navigation/Navigation.js

--- src/components/Header/Navigation/Navigation.js
+++ src/components/Header/Navigation/Navigation.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import './Navigation.css';
 
 
@@ -6,6 +7,7 @@ import './Navigation.css';
 function Navigation() {
   return (
     <nav className="navigation">
+      
     </nav>
   )
 }
