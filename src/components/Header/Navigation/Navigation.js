6a6bd6437f4669448512533d0a1db653efc85aea
Shnaider Danila <shnd3r@yandex.ru>
2023-10-10 13:50:24 +0700

    feat: Add a functional component to ./src/components/Header/Navigation/Navigation.js

---
+++ src/components/Header/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Navigation.css';
+
+
+
+function Navigation() {
+  return (
+    <nav className="navigation">
+    </nav>
+  )
+}
+
+export default Navigation;
