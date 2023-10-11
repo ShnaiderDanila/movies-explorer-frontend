649053bb51002e2d7d835c90809b6e8d5895c202
Shnaider Danila <shnd3r@yandex.ru>
2023-10-11 14:10:11 +0700

    feat: Connect the Main component to ./src/components/App/App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,15 +2,17 @@ import { useState } from 'react';
 import './App.css';
 
 import Header from '../Header/Header';
+import Main from '../Main/Main';
 
 function App() {
 
   // Временая стейт переменная авторизованного пользователя для верстки JSX
-  const [isLoggedIn] = useState(true);
+  const [isLoggedIn] = useState(false);
 
   return (
     <div className="wrapper">
       <Header isLoggedIn={isLoggedIn} />
+      <Main />
     </div>
   );
 }
