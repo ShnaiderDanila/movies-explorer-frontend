6da4e252f81086e1e1548fe1d22391a5ec9c3af3
Shnaider Danila <shnd3r@yandex.ru>
2023-10-11 16:09:08 +0700

    feat: Connect the AboutProject component to ./src/components/Main/Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,12 +3,14 @@ import './Main.css';
 
 import Promo from './Promo/Promo';
 import NavTab from './NavTab/NavTab';
+import AboutProject from './AboutProject/AboutProject';
 
 function Main() {
   return (
     <main className='main'>
       <Promo />
       <NavTab />
+      <AboutProject />
     </main>
   )
 }
