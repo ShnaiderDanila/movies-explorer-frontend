d9a0a2e30e34638ef2bd01dec28fd14dbfdc1b9f
Shnaider Danila <shnd3r@yandex.ru>
2023-10-11 15:00:33 +0700

    feat: Add jsx markup to ./src/components/Main/Promo/Promo.js

--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './Promo.css';
 
+import promoLogo from '../../../images/promo-logo.svg';
+
 function Promo() {
- 
+  return (
+    <section className='promo'>
+      <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
+      <img className='promo__logo' src={promoLogo} alt='Логотип секции promo' />
+    </section>
+  )
 }
 
 export default Promo;
