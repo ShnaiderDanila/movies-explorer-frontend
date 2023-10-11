3bb62063bcd33c5146b35aa09beb460a479723a5
Shnaider Danila <shnd3r@yandex.ru>
2023-10-11 14:01:01 +0700

    feat: Connect the Header component to the App functional component, add the isLoggedIn state variable and add jsx markup to ./src/components/App/App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
+import Header from '../Header/Header';
+
 function App() {
+
+  // Временая стейт переменная авторизованного пользователя для верстки JSX
+  const [isLoggedIn] = useState(true);
+
   return (
     <div className="wrapper">
+      <Header isLoggedIn={isLoggedIn} />
     </div>
   );
 }
