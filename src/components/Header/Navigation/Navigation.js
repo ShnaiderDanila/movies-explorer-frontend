778ebd1403bd270be8f0710cbc1828e23eb62ec9
Shnaider Danila <shnd3r@yandex.ru>
2023-10-11 13:50:35 +0700

    feat: Add Link from react-router-dom and add jsx markup to the Navigation functional component at ./src/components/Header/Navigation/Navigation.js

--- src/components/Header/Navigation/Navigation.js
+++ src/components/Header/Navigation/Navigation.js
@@ -1,15 +1,31 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import './Navigation.css';
 
+function Navigation({ isLoggedIn }) {
 
-
-function Navigation() {
-  return (
-    <nav className="navigation">
-      
-    </nav>
-  )
+  if (isLoggedIn) {
+    return (
+      <nav className="navigation">
+        <ul className="navigation__menu">
+          <li className="navigation__menu-item">
+            <NavLink to='/movies' className='navigation__link'>Фильмы</NavLink>
+          </li>
+          <li className="navigation__menu-item">
+            <NavLink to='/saved-movies' className='navigation__link'>Сохранённые фильмы</NavLink>
+          </li>
+        </ul>
+        <Link to='/profile' className="navigation__profile-button"></Link>
+      </nav>
+    )
+  } else {
+    return (
+      <nav className="auth-navigation">
+        <Link to='/signup' className='auth-navigation__link'>Регистрация</Link>
+        <Link to='/signin' className="auth-navigation__button">Войти</Link>
+      </nav>
+    )
+  }
 }
 
 export default Navigation;
