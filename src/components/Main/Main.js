1e8123c400717ceac2d2ee3b9e2de06fb9329a4f
Shnaider Danila <shnd3r@yandex.ru>
2023-10-12 15:16:46 +0700

    feat: Connect the AboutMe component to ./src/components/Main/Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,6 +5,7 @@ import Promo from './Promo/Promo';
 import NavTab from './NavTab/NavTab';
 import AboutProject from './AboutProject/AboutProject';
 import Techs from './Techs/Techs';
+import AboutMe from './AboutMe/AboutMe';
 
 function Main() {
   return (
@@ -13,6 +14,7 @@ function Main() {
       <NavTab />
       <AboutProject />
       <Techs />
+      <AboutMe />
     </main>
   )
 }
