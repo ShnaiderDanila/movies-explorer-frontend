44667bab5bfd8f8b5e2c5ecc9186a97861641511
Shnaider Danila <shnd3r@yandex.ru>
2023-10-12 19:03:08 +0700

    feat: Connect Portfolio component to ./src/components/Main/Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,6 +6,7 @@ import NavTab from './NavTab/NavTab';
 import AboutProject from './AboutProject/AboutProject';
 import Techs from './Techs/Techs';
 import AboutMe from './AboutMe/AboutMe';
+import Portfolio from './Portfolio/Portfolio';
 
 function Main() {
   return (
@@ -15,6 +16,7 @@ function Main() {
       <AboutProject />
       <Techs />
       <AboutMe />
+      <Portfolio />
     </main>
   )
 }
