b5caec97792004f7ac23259f2bd2bba52e89cd2d
Shnaider Danila <shnd3r@yandex.ru>
2023-10-12 14:01:56 +0700

    fix: Replace unnecessary semantic section tags with div and change the names of some classes in ./src/components/Main/AboutProject/AboutProject.js

--- src/components/Main/AboutProject/AboutProject.js
+++ src/components/Main/AboutProject/AboutProject.js
@@ -4,9 +4,9 @@ import './AboutProject.css';
 function AboutProject() {
   return (
     <section className='about-project' id='about-project'>
-      <h2 className='about-project__title'>О проекте</h2>
-      <div className='about-project__line'></div>
-      <section className='about-project__info'>
+      <h2 className='about-project__title main__subtitle'>О проекте</h2>
+      <div className='about-project__title-line main__subtitle-line'></div>
+      <div className='about-project__container'>
         <div className='about-project__description'>
           <h3 className='about-project__description-title'>
             Дипломный проект включал 5 этапов
@@ -25,13 +25,13 @@ function AboutProject() {
             которые нужно было соблюдать, чтобы успешно защититься.
           </p>
         </div>
-      </section>
-      <section className='about-project__time-line'>
+      </div>
+      <div className='about-project__time-line'>
         <div className='about-project__time-line-blue'>1 неделя</div>
         <div className='about-project__time-line-grey'>4 недели</div>
         <span className='about-project__time-line-signature'>Back-end</span>
         <span className='about-project__time-line-signature'>Front-end</span>  
-      </section>
+      </div>
     </section>
   )
 }
