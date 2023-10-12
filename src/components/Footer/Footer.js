3e24c71a09e50b4b3ba0feaf5e1545c8e690d634
Shnaider Danila <shnd3r@yandex.ru>
2023-10-12 20:41:13 +0700

    feat: Add JSX markup for the Footer component to ./src/components/Footer/Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,18 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className='footer'>
-      
+      <h2 className='footer__title'>Учебный проект Яндекс.Практикум х BeatFilm.</h2>
+      <div className='footer__copyright'>
+        <p className='footer__text'>© 2023</p>
+        <nav className='footer__nav'>
+          <a className='footer__link' href='https://practicum.yandex.ru' target='_blank' rel="noreferrer">
+            Яндекс.Практикум
+          </a>
+          <a className='footer__link' href='https://github.com/Shnd3r' target='_blank' rel="noreferrer">
+            Github
+          </a>
+        </nav>
+      </div>
     </footer>
   )
 }
