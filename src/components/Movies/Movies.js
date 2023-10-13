cda116ac3a1bf0df4ba232a19518956c0a7dc046
Shnaider Danila <shnd3r@yandex.ru>
2023-10-13 14:20:29 +0700

    feat: Connect the SearchForm component to ./src/components/Movies/Movies.js

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Movies.css';
 
+import SearchForm from './SearchForm/SearchForm';
+
 function Movies() {
   return (
     <main className='movies'>
-
+      <SearchForm />
     </main>
   )
 }
