88c04926624bd355fd6d5102a711b81ed8d24afc
Shnaider Danila <shnd3r@yandex.ru>
2023-10-13 18:24:15 +0700

    feat: Connect the FilterCheckbox component and add jsx markup to ./src/components/Movies/SearchForm/SearchForm.js

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './SearchForm.css';
 
+import FilterCheckbox from './FilterCheckbox/FilterCheckbox';
+
 function SearchForm() {
   return (
     <section className='search-form'>
-      
+      <form className='search-form__form'>
+        <input className='search-form__input' id='film' name='film' type='text' placeholder='Фильм' />
+        <div className='search-form__container'>
+          <button className='search-form__button' type='submit' />
+          <hr className='search-form__line' />
+          <FilterCheckbox />
+          <label className='search-form__text' for='switch'>Короткометражки</label>
+        </div>
+      </form>
     </section>
   )
 }
