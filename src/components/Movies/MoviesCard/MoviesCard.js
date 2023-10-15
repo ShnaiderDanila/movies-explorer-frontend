c9e9148245cd651f67b8ed3b0ed2133250db902b
Shnaider Danila <shnd3r@yandex.ru>
2023-10-15 13:46:01 +0700

    feat: Add the MoviesCard functional component to ./src/components/Movies/MoviesCard/MoviesCard.js

---
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MoviesCard.css';
+
+function MoviesCard() {
+  return (
+    <article className='movies-card'>
+
+    </article>
+  )
+}
+
+export default MoviesCard;
