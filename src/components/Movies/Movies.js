f44ddb0f74e7bc3f8c2d775ebd54eb8c03f001f7
Shnaider Danila <shnd3r@yandex.ru>
2023-10-15 15:15:03 +0700

    feat: Connect the MoviesCardList component to .src/components/Movies/Movies.js

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './Movies.css';
 
 import SearchForm from './SearchForm/SearchForm';
+import MoviesCardList from './MoviesCardList/MoviesCardList';
 
 function Movies() {
   return (
     <main className='movies'>
       <SearchForm />
+      <MoviesCardList />
     </main>
   )
 }
