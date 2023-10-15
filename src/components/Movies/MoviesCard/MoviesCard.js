4d36a1aaf4fa0a31b6961195fe9defa3f7f7ccf5
Shnaider Danila <shnd3r@yandex.ru>
2023-10-15 19:28:19 +0700

    feat: Add jsx markup to ./src/components/Movies/MoviesCard/MoviesCard.js

--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import { useState } from 'react';
 import './MoviesCard.css';
 
-function MoviesCard() {
-  return (
-    <article className='movies-card'>
+function MoviesCard({ card }) {
 
-    </article>
+  const [isMovieSaved, setIsMovieSaved] = useState(false);
+
+  function handleMovieSave() {
+    setIsMovieSaved(!isMovieSaved);
+  }
+
+  return (
+    <li className='movies-card'>
+      <img className='movies-card__image' src={card.image} alt='Обложка фильма' />
+      <div className='movies-card__description'>
+        <div className='movies-card__container'>
+          <h3 className='movies-card__title'>{card.name}</h3>
+          <button
+            className={`movies-card__save-button ${isMovieSaved && 'movies-card__save-button_enabled'}`}
+            type='button'
+            onClick={handleMovieSave}
+          />
+        </div>
+        <p className='movies-card__duration'>{card.duration}</p>
+      </div>
+    </li>
   )
 }
 
