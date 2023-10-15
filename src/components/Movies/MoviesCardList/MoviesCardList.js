203fdcac4240c564e40bfbfeab7a256d1de195d3
Shnaider Danila <shnd3r@yandex.ru>
2023-10-15 14:33:06 +0700

    feat: Add a temporary array of information for cards to ./src/components/Movies/MoviesCardList/MoviesCardList.js

--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -1,10 +1,50 @@
 import React from 'react';
 import './MoviesCardList.css';
 
+import movieImage from '../../../images/movie-image.jpg';
+
+// Временный массив карточек, без использования API
+const cards = [
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  }
+]
+
 function MoviesCardList() {
   return (
     <section className='movies-card-list'>
-      
 
     </section>
   )
