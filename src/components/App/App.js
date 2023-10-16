1c173f417e51e91c81d484066bba2ca775f635c2
Shnaider Danila <shnd3r@yandex.ru>
2023-10-16 13:44:53 +0700

    feat: Add the SavedMovies component to the /saved-movies route in ./src/components/App/App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
 import Footer from '../Footer/Footer';
 
 
@@ -19,6 +20,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Main/>} />
         <Route path='/movies' element={<Movies/>} />
+        <Route path='/saved-movies' element={<SavedMovies/>} />
       </Routes>
       <Footer />
     </div>
