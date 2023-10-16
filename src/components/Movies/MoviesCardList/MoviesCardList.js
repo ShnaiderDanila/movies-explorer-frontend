87e344cc54186520cdaf349373f3ca214d292482
Shnaider Danila <shnd3r@yandex.ru>
2023-10-16 14:26:34 +0700

    feat: Add a useLocation hook, add a temporary array of saved cards and update the jsx markup in ./src/components/MoviesCardList/MoviesCardList.js

--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+
 import './MoviesCardList.css';
 
 import movieImage from '../../../images/movie-image.jpg';
@@ -89,12 +91,37 @@ const cards = [
   },
 ]
 
+// Временный массив сохранненых карточек, без использования API
+const savedCards = [
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+]
+
 function MoviesCardList() {
+
+  const { pathname } = useLocation();
+
   return (
     <section className='movies-card-list'>
-       <ul className='movies-card-list__list'>
-        {
-          cards.map((card, i) => {
+      <ul className='movies-card-list__list'>
+        {pathname === '/saved-movies'
+          ? savedCards.map((card, i) => {
+            return <MoviesCard key={i} card={card} />
+          })
+          : cards.map((card, i) => {
             return <MoviesCard key={i} card={card} />
           })
         }
