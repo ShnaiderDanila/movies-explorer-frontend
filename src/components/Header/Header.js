d7b608c216dc84787b009c81af293a5c7093b9f5
Shnaider Danila <shnd3r@yandex.ru>
2023-10-16 19:00:25 +0700

    feat: Add conditional logic to add components based on a specific route to ./src/components/Footer/Footer.js and ./src/components/Header/Header.js.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,28 @@
 import React from "react";
-import { Link , useLocation} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import './Header.css';
 
 import logo from '../../images/logo.svg';
 
 import Navigation from './Navigation/Navigation';
 
-function Header({isLoggedIn}) {
+function Header({ isLoggedIn }) {
 
   const { pathname } = useLocation();
-
-  return (
-    <header className={`header ${pathname !== '/' && 'header_white'}`}>
-      <Link to='/' className='header__logo-link'><img src={logo} className="header__logo" alt="Логотип" /></Link>
-      <Navigation isLoggedIn={isLoggedIn}/>
-    </header>
-  )
+  
+  if (
+    pathname === '/' ||
+    pathname === '/movies' ||
+    pathname === '/saved-movies' ||
+    pathname === '/profile'
+  ) {
+    return (
+      <header className={`header ${pathname !== '/' && 'header_white'}`}>
+        <Link to='/' className='header__logo-link'><img src={logo} className="header__logo" alt="Логотип" /></Link>
+        <Navigation isLoggedIn={isLoggedIn} />
+      </header>
+    )
+  }
 }
 
 export default Header;
