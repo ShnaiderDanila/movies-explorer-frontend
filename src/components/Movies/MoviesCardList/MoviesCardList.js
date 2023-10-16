74da7c93cb422ee0ebca12751bf8cd56ea3665af
Shnaider Danila <shnd3r@yandex.ru>
2023-10-16 15:39:02 +0700

    feat: Connect the Preloader component, add the state variable isLoading and update the jsx markup in ./src/components/Movies/MoviesCardList/MoviesCardList.js

--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import './MoviesCardList.css';
 
 import movieImage from '../../../images/movie-image.jpg';
 
+import Preloader from '../Preloader/Preloader';
 import MoviesCard from '../MoviesCard/MoviesCard';
 
 // Временный массив карточек, без использования API
@@ -114,21 +115,27 @@ function MoviesCardList() {
 
   const { pathname } = useLocation();
 
-  return (
-    <section className='movies-card-list'>
-      <ul className='movies-card-list__list'>
-        {pathname === '/saved-movies'
-          ? savedCards.map((card, i) => {
-            return <MoviesCard key={i} card={card} />
-          })
-          : cards.map((card, i) => {
-            return <MoviesCard key={i} card={card} />
-          })
-        }
-      </ul>
-      <button className='movies-card-list__button-more'>Ещё</button>
-    </section>
-  )
+  // Временая стейт переменная для отображения Прелоадера
+  const [isLoading] = useState(false);
+
+  if (isLoading) {
+    return <Preloader />
+  } else {
+    return (
+      <section className='movies-card-list'>
+        <ul className='movies-card-list__list'>
+          {pathname === '/saved-movies'
+            ? savedCards.map((card, i) => {
+              return <MoviesCard key={i} card={card} />
+            })
+            : cards.map((card, i) => {
+              return <MoviesCard key={i} card={card} />
+            })}
+        </ul>
+        <button className='movies-card-list__button-more'>Ещё</button>
+      </section>
+    )
+  }
 }
 
 export default MoviesCardList;
