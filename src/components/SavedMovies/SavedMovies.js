fb111721485ebe84371a28fa2f28e6be0703b7e2
Shnaider Danila <shnd3r@yandex.ru>
2023-10-16 13:40:58 +0700

    feat: Add the SavedMovies component to ./src/components/SavedMovies/SavedMovies.js

---
+++ src/components/SavedMovies/SavedMovies.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SavedMovies.css';
+
+function SavedMovies() {
+  return (
+    <main className='saved-movies'>
+
+    </main>
+  )
+}
+
+export default SavedMovies;
