df58bd68cac723df7ca34cc40e2f46d4657a6b70
Shnaider Danila <shnd3r@yandex.ru>
2023-10-16 18:58:16 +0700

    feat: Connect the Profile component to .src/components/App/App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,7 @@ import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
 import Footer from '../Footer/Footer';
 
 
@@ -13,15 +14,16 @@ function App() {
 
   // Временая стейт переменная авторизованного пользователя, 
   // для изменения отображения jsx верстки компонента Header
-  const [isLoggedIn] = useState(false);
+  const [isLoggedIn] = useState(true);
 
   return (
     <div className="wrapper">
       <Header isLoggedIn={isLoggedIn} />
       <Routes>
-        <Route path='/' element={<Main/>} />
-        <Route path='/movies' element={<Movies/>} />
-        <Route path='/saved-movies' element={<SavedMovies/>} />
+        <Route path='/' element={<Main />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/saved-movies' element={<SavedMovies />} />
+        <Route path='/profile' element={<Profile />} />
       </Routes>
       <Footer />
     </div>
