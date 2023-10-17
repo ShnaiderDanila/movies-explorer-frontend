a5ab3eb38a494d8688ab2849443ff86327bd2c37
Shnaider Danila <shnd3r@yandex.ru>
2023-10-17 15:03:39 +0700

    feat: Connect the Register component to the signup route in ./src/components/App/App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,6 +7,7 @@ import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
 import Profile from '../Profile/Profile';
+import Register from '../Register/Register';
 import Footer from '../Footer/Footer';
 
 
@@ -24,6 +25,7 @@ function App() {
         <Route path='/movies' element={<Movies />} />
         <Route path='/saved-movies' element={<SavedMovies />} />
         <Route path='/profile' element={<Profile />} />
+        <Route path='/signup' element={<Register />} />
       </Routes>
       <Footer />
     </div>
