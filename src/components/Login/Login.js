cd881fb484b3726b046a4008d488d1b9e16fd465
Shnaider Danila <shnd3r@yandex.ru>
2023-10-18 14:36:45 +0700

    feat: Add Login component to ./src/components/Login/Login.js

---
+++ src/components/Login/Login.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css';
+
+function Login() {
+
+  return (
+    <main className='login'>
+
+    </main>
+  )
+}
+
+export default Login;
