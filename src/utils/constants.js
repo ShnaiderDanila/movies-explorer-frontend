2506ea1385a214266e76f3f69616ed4a770fbcde
Shnaider Danila <shnd3r@yandex.ru>
2023-10-21 19:51:45 +0700

    feat: Move the cards and savedCards arrays from ./src/components/Movies/MoviesCardList/MoviesCardList.js to ./src/utils/constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,106 @@
+import movieImage from '../images/movie-image.jpg';
+
+// Временный массив карточек, без использования API
+const cards = [
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+]
+
+// Временный массив сохранненых карточек, без использования API
+const savedCards = [
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+  {
+    image: movieImage,
+    name: '33 слова о дизайне',
+    duration: '1ч42м',
+  },
+]
+
+export { cards, savedCards };
