c27d02017b52482f23123d60cca372fba5090236
Shnaider Danila <shnd3r@yandex.ru>
2023-10-24 19:30:47 +0700

    feat: Add useValidation.js hook to src/hooks/useValidation.js

---
+++ src/hooks/useValidation.js
@@ -0,0 +1,36 @@
+import { useCallback, useState } from 'react';
+
+function useValidation () {
+  // Стейт-переменная значения инпута
+  const [inputValues, setInputValues] = useState({});
+  // Стейт-переменная текста ошибки валидации
+  const [errorMessages, setErrorMessages] = useState({});
+    // Стейт-переменная валидности формы 
+  const [isValidForm, setIsValidForm] = useState(true);
+
+  // Функция обработки валидации при срабатывании события
+  function handleChangeValidation(evt) {
+    const {name, value} = evt.target;
+    const error = evt.target.validationMessage;
+    const isValidForm = evt.target.closest("form").checkValidity();
+    setInputValues({ ...inputValues, [name]: value});
+    setErrorMessages({ ...errorMessages, [name]: error});
+    setIsValidForm(isValidForm);
+  };
+
+  const resetValidation = useCallback((newInputValues={}, newErrorMessages={}, newIsValidForm = false) => {
+    setInputValues(newInputValues);
+    setErrorMessages(newErrorMessages);
+    setIsValidForm(newIsValidForm);
+  }, [setInputValues, setErrorMessages, setIsValidForm]);
+
+  return {
+    inputValues,
+    errorMessages,
+    isValidForm,
+    handleChangeValidation,
+    resetValidation,
+  }
+}
+
+export default useValidation;
