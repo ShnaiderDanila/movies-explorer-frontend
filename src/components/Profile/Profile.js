d0492ac59f211b7cbb045339b8540b5cc95f83c5
Shnaider Danila <shnd3r@yandex.ru>
2023-10-24 20:38:06 +0700

    feat: Implement the validation logic for the profile editing page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,89 +1,114 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './Profile.css';
+
+import useValidation from '../../hooks/useValidation';
 
 function Profile() {
 
-  // Временные стейт переменные для проверки JSX верстки
-  const [name, setName] = useState('Виталий');
-  const [email, setEmail] = useState('pochta@yandex.ru');
+  const [name] = useState('Данил');
+  const [email] = useState('dan_shnai2001@mail.ru');
+
+  const {
+    inputValues,
+    errorMessages,
+    isValidForm,
+    handleChangeValidation,
+    resetValidation
+  } = useValidation();
+
   const [formIsEditing, setFormIsEditing] = useState(false);
-  
-  // Иммитация возникшей ошибки, при редактировании профиля 
-  const [editingError] = useState(false);
+
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  useEffect(() => {
+    resetValidation({ email: email, name: name });
+  }, []);
+
+  useEffect(() => {
+    if (name !== inputValues.name || email !== inputValues.email) {
+      setIsDisabled(true);
+    } else {
+      setIsDisabled(false);
+    }
+  }, [inputValues, isValidForm]);
 
   // Временные функции для проверки JSX верстки
   function editProfileForm() {
     setFormIsEditing(true);
   };
 
-  function handleChangeName(evt) {
-    setName(evt.target.value);
-  };
-
-  function handleChangeEmail(evt) {
-    setEmail(evt.target.value);
-  };
-
   return (
     <main className='profile'>
       <h2 className='profile__title'>Привет, Виталий!</h2>
-      <form className='profile__form'>
+      <form className='profile__form' noValidate>
         <fieldset className='profile__fieldset'>
-          <label className='profile__label'>
-            <span className='profile__label-name'>Имя</span>
-            <input
-              className='profile__input'
-              name='name'
-              type='text'
-              placeholder='Имя'
-              required
-              minLength='2'
-              maxLength='10'
-              readOnly={!formIsEditing}
-              value={name || ''}
-              onChange={handleChangeName}>
-            </input>
-          </label>
-          <label className='profile__label'>
-            <span className='profile__label-name'>E-mail</span>
-            <input
-              className='profile__input'
-              name='email'
-              type='email'
-              placeholder='E-mail'
-              required
-              readOnly={!formIsEditing}
-              value={email || ''}
-              onChange={handleChangeEmail}>
-            </input>
-          </label>
+          <div className='profile__label-container'>
+            <label className='profile__label'>
+              <span className='profile__label-name'>Имя</span>
+              <input
+                className='profile__input'
+                name='name'
+                placeholder='Имя'
+                type='text'
+                required
+                minLength='2'
+                maxLength='10'
+                pattern="^[A-Za-zА-Яа-яЁё\-\s]+$"
+                readOnly={!formIsEditing}
+                value={inputValues.name || ''}
+                onChange={handleChangeValidation}>
+              </input>
+            </label>
+            <span
+              className={`profile__input-error ${!isValidForm && 'profile__input-error_active'}`}>
+              {errorMessages.name}
+            </span>
+          </div>
+          <div className='profile__label-container'>
+            <label className='profile__label'>
+              <span className='profile__label-name'>E-mail</span>
+              <input
+                className='profile__input'
+                name='email'
+                placeholder='E-mail'
+                type='email'
+                required
+                pattern="^[a-zA-Z0-9_.+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+$"
+                readOnly={!formIsEditing}
+                value={inputValues.email || ''}
+                onChange={handleChangeValidation}>
+              </input>
+            </label>
+            <span
+              id='email' className={`profile__input-error ${!isValidForm && 'profile__input-error_active'}`}>
+              {errorMessages.email}
+            </span>
+          </div>
         </fieldset>
-        {formIsEditing
-          ? <>
-            <p className={`profile__warning ${editingError && 'profile__warning_enabled'}`}>
-              При обновлении профиля произошла ошибка.
-            </p>
-            <button
-              className={`profile__button profile__button-save ${editingError && 'profile__button-save_disabled'}`}
-              type='submit'
-              disabled={editingError}>
-              Сохранить
-            </button>
-          </>
-          : <div className='profile__buttons'>
-            <button
+        {
+          formIsEditing
+            ? <div className='profile__container'>
+              <p className={`profile__warning ${!isValidForm && 'profile__warning_enabled'}`}>
+              </p>
+              <button
+                className={`profile__button profile__button-save ${!isDisabled && 'profile__button-save_disabled'}`}
+                type='submit'
+                disabled={!isDisabled || !isValidForm}>
+                Сохранить
+              </button>
+            </div>
+            : <button
               className='profile__button profile__button-edit'
               type='button'
               onClick={editProfileForm}>
               Редактировать
             </button>
-            <button
-              className='profile__button profile__button-exit'
-              type='button'>
-              Выйти из аккаунта
-            </button>
-          </div>
         }
+        <button
+          className='profile__button profile__button-exit'
+          type='button'>
+          Выйти из аккаунта
+        </button>
       </form>
     </main>
   )
