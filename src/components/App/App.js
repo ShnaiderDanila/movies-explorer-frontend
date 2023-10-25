5065d8d1df046673f5f7e4a186b7220153221998
Shnaider Danila <shnd3r@yandex.ru>
2023-10-25 21:28:06 +0700

    feat: Connect CurrentUserContext in src/components/App/App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,10 @@
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { useState, useEffect, useCallback } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { mainApi } from '../../utils/MainApi';
 
 import './App.css';
 
+import CurrentUserContext from "../../contexts/CurrentUserContext";
 import Header from '../Header/Header';
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 import Main from '../Main/Main';
@@ -21,36 +23,39 @@ function App() {
   // Cтейт переменная авторизованного пользователя, 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const [currentUser, setCurrentUser] = useState({});
+
   // Стейт-переменные состояния попапа infoTooltip
   const [isInfoTooltipOpen, setIsInfoTooltipOpen] = useState(false);
   const [infoTooltipTitle, setInfoTooltipTitle] = useState('');
 
-  // Стейт переменная серверных ошибок для форм
   const [serverError, setServerError] = useState('');
 
   return (
     <div className="wrapper">
-      <Header isLoggedIn={isLoggedIn} />
-      <Routes>
-        <Route path='*' element={<NotFound />} />
-        <Route path='/' element={<Main />} />
-        <Route path='/movies' element=
-          {
-            <ProtectedRoute
-              element={Movies}
-              isLoggedIn={isLoggedIn} />
-          } />
-        <Route path='/saved-movies' element={<ProtectedRoute element={SavedMovies} isLoggedIn={isLoggedIn} />} />
-        <Route path='/profile' element={<ProtectedRoute element={Profile} isLoggedIn={isLoggedIn} />} />
-        <Route path='/signup' element={<Register/>} />
-        <Route path='/signin' element={<Login />} />
-      </Routes>
-      <Footer />
-      <InfoTooltip
-        setIsInfoTooltipOpen={setIsInfoTooltipOpen}
-        isInfoTooltipOpen={isInfoTooltipOpen}
-        infoTooltipTitle={infoTooltipTitle} />
-    </div>
+      <CurrentUserContext.Provider value={currentUser}>
+        <Header isLoggedIn={isLoggedIn} />
+        <Routes>
+          <Route path='*' element={<NotFound />} />
+          <Route path='/' element={<Main />} />
+          <Route path='/movies' element=
+            {
+              <ProtectedRoute
+                element={Movies}
+                isLoggedIn={isLoggedIn} />
+            } />
+          <Route path='/saved-movies' element={<ProtectedRoute element={SavedMovies} isLoggedIn={isLoggedIn} />} />
+          <Route path='/profile' element={<ProtectedRoute element={Profile} isLoggedIn={isLoggedIn} />} />
+          <Route path='/signup' element={<Register />} />
+          <Route path='/signin' element= {<Login />} />
+        </Routes>
+        <Footer />
+        <InfoTooltip
+          setIsInfoTooltipOpen={setIsInfoTooltipOpen}
+          isInfoTooltipOpen={isInfoTooltipOpen}
+          infoTooltipTitle={infoTooltipTitle} />
+      </CurrentUserContext.Provider>
+    </div >
   );
 }
 
