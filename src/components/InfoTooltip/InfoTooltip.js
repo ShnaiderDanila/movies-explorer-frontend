09d4c8a5b88832c7311cdcd2b631fa55eea65456
Shnaider Danila <shnd3r@yandex.ru>
2023-10-25 19:38:56 +0700

    feat: Add the InfoTooltip component with css styles to src/components/InfoTooltip

---
+++ src/components/InfoTooltip/InfoTooltip.js
@@ -0,0 +1,30 @@
+import { React } from 'react';
+
+import './InfoTooltip.css';
+
+// Импортируем иконки
+import iconSuccess from '../../images/icon-success.svg';
+
+function InfoTooltip({ setIsInfoTooltipOpen, isInfoTooltipOpen, infoTooltipTitle }) {
+
+  function closeInfoTooltip() {
+    setIsInfoTooltipOpen(false);
+  }
+
+  return (
+    <div className={`info-tooltip ${isInfoTooltipOpen ? 'info-tooltip_is-opened' : ''}`}>
+      <div className="info-tooltip__container">
+        <img
+          className="info-tooltip__image"
+          src={iconSuccess}
+          alt='Информационная всплывающая подсказка' />
+        <h3 className="info-tooltip__title">
+          {infoTooltipTitle}
+        </h3>
+        <button onClick={closeInfoTooltip} className="info-tooltip__button-close" type="button" />
+      </div>
+    </div>
+  )
+}
+
+export default InfoTooltip
