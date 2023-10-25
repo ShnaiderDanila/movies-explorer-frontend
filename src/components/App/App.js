412224b89b15fc8a75ef51ecd92ad743836f8054
Shnaider Danila <shnd3r@yandex.ru>
2023-10-25 19:41:00 +0700

    feat: Connect the InfoTooltip component in src/components/App/App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
 
 import Header from '../Header/Header';
@@ -12,12 +13,20 @@ import Register from '../Register/Register';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
 import NotFound from '../NotFound/NotFound';
+import InfoTooltip from '../InfoTooltip/InfoTooltip';
 
 
 function App() {
 
   // Cтейт переменная авторизованного пользователя, 
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Стейт-переменные состояния попапа infoTooltip
+  const [isInfoTooltipOpen, setIsInfoTooltipOpen] = useState(false);
+  const [infoTooltipTitle, setInfoTooltipTitle] = useState('');
+
+  // Стейт переменная серверных ошибок для форм
+  const [serverError, setServerError] = useState('');
 
   return (
     <div className="wrapper">
@@ -33,10 +42,14 @@ function App() {
           } />
         <Route path='/saved-movies' element={<ProtectedRoute element={SavedMovies} isLoggedIn={isLoggedIn} />} />
         <Route path='/profile' element={<ProtectedRoute element={Profile} isLoggedIn={isLoggedIn} />} />
-        <Route path='/signup' element={<Register />} />
+        <Route path='/signup' element={<Register/>} />
         <Route path='/signin' element={<Login />} />
       </Routes>
       <Footer />
+      <InfoTooltip
+        setIsInfoTooltipOpen={setIsInfoTooltipOpen}
+        isInfoTooltipOpen={isInfoTooltipOpen}
+        infoTooltipTitle={infoTooltipTitle} />
     </div>
   );
 }
