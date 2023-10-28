eb37ad00eecb5a48b12371e3f5dc919de034a159
Shnaider Danila <shnd3r@yandex.ru>
2023-10-28 18:50:24 +0700

    fix: change base Url in src/utils/MainApi.js

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -145,7 +145,7 @@ class MainApi {
 }
 
 const mainApi = new MainApi({
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'https://api.movies-explorer.shndr.nomoredomainsrocks.ru',
   headers: {
     'Content-Type': 'application/json'
   },
